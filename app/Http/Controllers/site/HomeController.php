9551f03829ed26669f5b8b3d908d3e4181f58e7c
levannghia <levannghia.tdc2018@gmail.com>
2022-03-01 21:26:42 +0700

    Lai

--- app/Http/Controllers/site/HomeController.php
+++ app/Http/Controllers/site/HomeController.php
@@ -27,7 +27,7 @@ class HomeController extends Controller
             "width" => 110,
             "height" => 110,
         ]));
-        $category_lv1 = Category_LV1::where('categories_lv1.status', 1)->where('categories_lv1.noi_bac', 1)->orderBy('categories_lv1.stt', 'ASC')->get();
+        $category_lv1 = Category_LV1::where('categories_lv1.status', 1)->where('categories_lv1.noi_bac', 1)->orderBy('categories_lv1.stt', 'ASC')->limit(2)->get();
         $news = News::where('status', 1)->where('noi_bac', 1)->orderBy('id', 'DESC')->limit(2)->get();
         $standard = Standard::where('status', 1)->orderBy('stt', 'ASC')->get();
         $new_product = Products::where('status', 1)->orderBy('id', 'DESC')->where('type', 0)->limit(5)->get();
--- resources/views/site/home/index.blade.php
+++ resources/views/site/home/index.blade.php
@@ -58,23 +58,41 @@
                         ->where('categories_lv1.id', $value->id)
                         ->orderBy('categories.stt', 'ASC')
                         ->get();
+                    
                     ?>
                     @if ($key == 0)
                         @foreach ($category_noibac as $item)
                             <span class="tablinks{{ $key }}" category-id="{{ $item->id }}"
-                                onclick="chemistry(event, 'chemistry-{{ $item->id }}')">{{ $item->name }}</span>
+                                onclick="chemistry(event, 'chemistry-{{ $item->id }}')">{{ $item->name }}
+                            </span>
+                            {{ session(['id_cate1' => $item->id]) }}
                         @endforeach
                     @else
                         @foreach ($category_noibac as $item)
                             <span class="tablinks{{ $key }}" category-id="{{ $item->id }}"
-                                onclick="medicalEquipment(event, 'medical-equipment-{{ $item->id }}')">{{ $item->name }}</span>
+                                onclick="medicalEquipment(event, 'medical-equipment-{{ $item->id }}')">{{ $item->name }}
+                            </span>
+                            {{ session(['id_cate2' => $item->id]) }}
                         @endforeach
                     @endif
+
                 </div>
                 <br>
                 @if ($key == 0)
                     @foreach ($category_noibac as $item)
+                        {{-- @php
+                            $cate_product = DB::table('products')
+                                ->select('products.id', 'products.name', 'products.price', 'products.view', 'products.photo', 'categories.name AS category_name')
+                                ->join('categories', 'categories.id', '=', 'products.category_id')
+                                ->where('categories.id', $item->id)
+                                ->where('products.type', 0)
+                                ->where('products.status', 1)
+                                ->orderBy('categories.stt', 'ASC')
+                                ->paginate($settings['PHAN_TRANG_PRODUCT']);
+                            
+                        @endphp --}}
                         <div id="chemistry-{{ $item->id }}" class="tab-content{{ $key }}">
+
                         </div>
                     @endforeach
                 @else
@@ -413,14 +431,14 @@
                                         <g>
                                             <path style="fill:white;"
                                                 d="M118.1,235.517c7.898,0,14.31-6.032,14.31-13.483c0-7.441,0-13.473,0-13.473
-                                                                        c39.069-3.579,64.932-24.215,64.932-57.785v-0.549c0-34.119-22.012-49.8-65.758-59.977V58.334c6.298,1.539,12.82,3.72,19.194,6.549
-                                                                        c10.258,4.547,22.724,1.697,28.952-8.485c6.233-10.176,2.866-24.47-8.681-29.654c-11.498-5.156-24.117-8.708-38.095-10.236V8.251
-                                                                        c0-4.552-6.402-8.251-14.305-8.251c-7.903,0-14.31,3.514-14.31,7.832c0,4.335,0,7.843,0,7.843
-                                                                        c-42.104,3.03-65.764,25.591-65.764,58.057v0.555c0,34.114,22.561,49.256,66.862,59.427v33.021
-                                                                        c-10.628-1.713-21.033-5.243-31.623-10.65c-11.281-5.755-25.101-3.72-31.938,6.385c-6.842,10.1-4.079,24.449,7.294,30.029
-                                                                        c16.709,8.208,35.593,13.57,54.614,15.518v13.755C103.79,229.36,110.197,235.517,118.1,235.517z M131.301,138.12
-                                                                        c14.316,4.123,18.438,8.257,18.438,15.681v0.555c0,7.979-5.776,12.651-18.438,14.033V138.12z M86.999,70.153v-0.549
-                                                                        c0-7.152,5.232-12.657,18.71-13.755v29.719C90.856,81.439,86.999,77.305,86.999,70.153z" />
+                                                                                c39.069-3.579,64.932-24.215,64.932-57.785v-0.549c0-34.119-22.012-49.8-65.758-59.977V58.334c6.298,1.539,12.82,3.72,19.194,6.549
+                                                                                c10.258,4.547,22.724,1.697,28.952-8.485c6.233-10.176,2.866-24.47-8.681-29.654c-11.498-5.156-24.117-8.708-38.095-10.236V8.251
+                                                                                c0-4.552-6.402-8.251-14.305-8.251c-7.903,0-14.31,3.514-14.31,7.832c0,4.335,0,7.843,0,7.843
+                                                                                c-42.104,3.03-65.764,25.591-65.764,58.057v0.555c0,34.114,22.561,49.256,66.862,59.427v33.021
+                                                                                c-10.628-1.713-21.033-5.243-31.623-10.65c-11.281-5.755-25.101-3.72-31.938,6.385c-6.842,10.1-4.079,24.449,7.294,30.029
+                                                                                c16.709,8.208,35.593,13.57,54.614,15.518v13.755C103.79,229.36,110.197,235.517,118.1,235.517z M131.301,138.12
+                                                                                c14.316,4.123,18.438,8.257,18.438,15.681v0.555c0,7.979-5.776,12.651-18.438,14.033V138.12z M86.999,70.153v-0.549
+                                                                                c0-7.152,5.232-12.657,18.71-13.755v29.719C90.856,81.439,86.999,77.305,86.999,70.153z" />
                                         </g>
                                     </svg>
                                     @if ($item->price == null)
@@ -743,6 +761,42 @@
 @push('script_site')
     <script>
         $(document).ready(function() {
+            loadCate1();
+            loadCate2()
+
+            function loadCate1() {
+                let id = "{{ session()->get('id_cate1') }}";
+                var show_product = $("#chemistry-" + id);
+
+                $.ajax({
+                    method: "GET",
+                    url: "{{ route('show.product.category') }}",
+                    data: {
+                        id: id
+                    },
+                    success: function(data) {
+                        show_product.html(data);
+                    }
+                });
+            }
+
+            function loadCate2() {
+                let id = "{{ session()->get('id_cate2') }}";
+                var show_product = $("#medical-equipment-" + id);
+
+                $.ajax({
+                    method: "GET",
+                    url: "{{ route('show.product.category') }}",
+                    data: {
+
+                        id: id
+                    },
+                    success: function(data) {
+                        show_product.html(data);
+                    }
+                });
+            }
+
             $("[category-id]").click(function() {
                 let id = $(this).attr('category-id');
                 var show_product = $("#chemistry-" + id);
@@ -753,7 +807,7 @@
                     method: "GET",
                     url: "{{ route('show.product.category') }}",
                     data: {
-                        
+
                         id: id
                     },
                     success: function(data) {
@@ -772,7 +826,7 @@
                     method: "GET",
                     url: "{{ route('show.product.category') }}",
                     data: {
-                        
+
                         id: id
                     },
                     success: function(data) {
